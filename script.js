e53a9d6a9996205bacfcca6da5a9faec87921eb1
NajYehoshua <112788527+NajYehoshua@users.noreply.github.com>
2024-12-17 15:21:56 +0800

    Partial Implementation of basic four function operation
    I partially implemented the basic four functions application logic of the app. However, I have a problem how I implemented the second operand number of the operation.
    git commit -m

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function app() {
   );
 
   //* App Level variables
-  let operandOne = 0;
-  let operandTwo = 0;
+  let operandOne;
+  let operandTwo;
   let operator = null;
+  let previousFirstInputedNum = [];
+  let previousSecondInputedNum = [];
 
   //* Calculator Object
   const calculator = {
@@ -32,7 +34,11 @@ function app() {
     },
   };
 
-  //! Operate
+  function returnAnOperator(operator) {
+    return operator;
+  }
+
+  //! Operation function
   function operate(num1, num2, operator) {
     let result = 0;
 
@@ -70,7 +76,64 @@ function app() {
     }
   }
 
-  console.log(operate(1, 2, "divide"));
+  // console.log(operate(1, 2, "divide"));
+
+  //! Attach an event listener to number buttons container
+  numInputContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-num")) {
+      //* Get the input number
+      let userNumber = e.target.textContent;
+
+      //* Append the number to the previous inputed number
+      previousFirstInputedNum.push(userNumber);
+
+      //* Assigned the the first inputed number to first operand varialbe
+      operandOne = +previousFirstInputedNum.join("");
+
+      //* Display the number to the UI
+      displayContainer.textContent = "";
+      displayContainer.textContent += +previousFirstInputedNum.join("");
+
+      console.log(operandOne);
+    }
+  });
+
+  //! Operation input add event listener
+  operationInputContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-operation")) {
+      //* Get the specified user operation
+      const userSpecifiedOperation = e.target.textContent.trim();
+
+      //* check the operation if addition
+      if (userSpecifiedOperation === "+") {
+        operator = "addition";
+
+        //* Clear Display Container
+        displayContainer.textContent = "";
+      }
+
+      //* check the operation if addition
+      if (userSpecifiedOperation === "-") {
+        operator = "subtraction";
+
+        console.log(operator);
+      }
+
+      //* check the operation if addition
+      if (userSpecifiedOperation === "/") {
+        operator = "division";
+
+        console.log(operator);
+      }
+
+      //* check the operation if addition
+      if (userSpecifiedOperation === "X") {
+        operator = "multiply";
+
+        console.log(operator);
+      }
+    }
+  });
 }
 
 app();
